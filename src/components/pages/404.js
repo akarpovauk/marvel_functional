c4f2d30a21079e710860d339e57f6898527a75da
Alena <alena.karpov@gmail.com>
2025-04-03 09:02:08 +0100

    SEO: helmet: add meta-tags to 404 page

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,16 @@
 import ErrorMessage from "../errorMessage/errorMessage";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
 	return (
 		<div>
+			<Helmet>
+				<meta
+					name="description"
+					content="This page is not found"/>
+				<title>This page is not found</title>
+			</Helmet>
 			<ErrorMessage/>
 			<p style={{'textAlign': 'center',
 						'fontWeight': 'bold',
